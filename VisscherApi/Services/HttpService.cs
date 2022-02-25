3d8f9695abec88da2b7e21744b4f2049b98ca23b
Hannah Young <hannah@corgibyte.com>
2022-02-25 13:55:37 -0800

    Implement HttpService

---
+++ VisscherApi/Services/HttpService.cs
@@ -0,0 +1,47 @@
+using VisscherApi.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VisscherApi.Services;
+
+public class HttpService
+{
+  private readonly VisscherApiContext _db;
+  private System.Threading.Timer _timer = null;
+  private Queue<IScrapeable> _queue;
+  private readonly HttpClient _client;
+
+  public HttpService(IServiceProvider provider)
+  {
+    _queue = new Queue<IScrapeable>();
+    _db = provider.GetRequiredService<VisscherApiContext>();
+    _client = new HttpClient();
+#pragma warning disable 4014
+    StartAsync();
+  }
+
+  private async Task StartAsync()
+  {
+    _timer = new Timer(IncrementQueue, null, TimeSpan.Zero, TimeSpan.FromSeconds(10));
+    await Task.CompletedTask;
+  }
+
+  private async void IncrementQueue(object state)
+  {
+    if (_queue.Count != 0)
+    {
+      IScrapeable thisScrapeable = _queue.Dequeue();
+      string htmlResponse = await _client.GetStringAsync(thisScrapeable.Url);
+      thisScrapeable.Parse(htmlResponse, _db);
+    }
+  }
+
+  public void AddToQueue(IScrapeable scrapeable)
+  {
+    _queue.Enqueue(scrapeable);
+  }
+}
--- VisscherApi/Startup.cs
+++ VisscherApi/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 using VisscherApi.Models;
+using VisscherApi.Services;
 
 namespace VisscherApi;
 
@@ -32,6 +33,7 @@ public class Startup
     {
       c.SwaggerDoc("v1", new OpenApiInfo { Title = "Visscher API", Version = "v1" });
     });
+    services.AddSingleton<HttpService>();
   }
 
   // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
