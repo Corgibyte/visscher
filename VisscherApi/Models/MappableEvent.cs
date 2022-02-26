5ab9413ba8c9d75e31d9f30f912f8795c3b1fc2c
Hannah Young <hannah@corgibyte.com>
2022-02-26 14:54:17 -0800

    Update MappableEvent model

--- VisscherApi/Models/MappableEvent.cs
+++ VisscherApi/Models/MappableEvent.cs
@@ -17,21 +17,44 @@ public abstract class MappableEvent : IScrapeable
   public string Name { get; set; }
 
   [Required]
-  public string Description { get; set; }
-
-  [Required]
-  public DateTime Date { get; set; }
+  public int Year { get; set; }
 
   public string Url { get; set; }
 
   public DateTime LastChecked { get; set; }
 
-  public abstract bool Parse(string html, VisscherApiContext db);
-
-  public abstract bool Update(HttpService httpService);
-
   public int CategoryId { get; set; }
 
   [Key]
   public int EventId { get; set; }
+
+  public abstract bool Parse(string html, VisscherApiContext db);
+
+  public abstract bool Update(HttpService httpService);
+
+  public float ParseLatOrLongString(string latOrLong)
+  {
+    string[] workingArray = latOrLong.Split('°');
+    float degrees = float.Parse(workingArray[0]);
+    workingArray = workingArray[1].Split('′');
+    float minutes = float.Parse(workingArray[0]);
+    workingArray = workingArray[1].Split('″');
+    float seconds = float.Parse(workingArray[0]);
+    float value = degrees + (minutes / 60) + (seconds / 3600);
+    return workingArray[1] == "N" || workingArray[1] == "W" ? value : value * -1;
+  }
+
+  public int ParseDateForYear(string date)
+  {
+    string[] words = date.Split(" ");
+    foreach (string word in words)
+    {
+      int year;
+      if (word.Length >= 3 && int.TryParse(word, out year))
+      {
+        return year;
+      }
+    }
+    return -1;
+  }
 }
