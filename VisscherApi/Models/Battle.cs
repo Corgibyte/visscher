ebf867fe7f546dea8e9f8a65d802153878e0c9ac
Hannah Young <hannah@corgibyte.com>
2022-02-26 21:55:16 -0800

    BUGFIX: Fix parse exceptions

--- VisscherApi/Models/Battle.cs
+++ VisscherApi/Models/Battle.cs
@@ -12,45 +12,52 @@ public class Battle : MappableEvent
   {
     HtmlDocument htmlDoc = new HtmlDocument();
     htmlDoc.LoadHtml(html);
-    string latitude = htmlDoc.DocumentNode.Descendants()
-      .Where(node => node.GetAttributeValue("class", "").Contains("latitude"))
-      .FirstOrDefault()
-      .InnerText;
-    string longitude = htmlDoc.DocumentNode.Descendants()
-      .Where(node => node.GetAttributeValue("class", "").Contains("longitude"))
-      .FirstOrDefault()
-      .InnerText;
-    string name = htmlDoc.DocumentNode.Descendants()
-      .Where(node => node.GetAttributeValue("class", "").Contains("firstHeading"))
-      .FirstOrDefault()
-      .InnerText;
-    ParseResult dateParse = ParseDateForYear(htmlDoc.DocumentNode.Descendants("tr")
-      .Where(node => node.FirstChild.InnerText == "Date")
-      .FirstOrDefault().Descendants("td")
-      .FirstOrDefault().InnerText);
-    int year = -1;
-    if (dateParse.Result)
+    if (htmlDoc.DocumentNode != null)
     {
-      year = int.Parse(dateParse.Message);
-    }
-    //* Only save entity to database if it has found a lat, long, and year
-    if (latitude != "" && longitude != "" && name != "" && year != -1)
-    {
-      Battle newBattle = new Battle()
+      string latitude = htmlDoc.DocumentNode.Descendants()
+        .Where(node => node.GetAttributeValue("class", "").Contains("latitude"))
+        .FirstOrDefault()
+        .InnerText;
+      string longitude = htmlDoc.DocumentNode.Descendants()
+        .Where(node => node.GetAttributeValue("class", "").Contains("longitude"))
+        .FirstOrDefault()
+        .InnerText;
+      string name = htmlDoc.DocumentNode.Descendants()
+        .Where(node => node.GetAttributeValue("class", "").Contains("firstHeading"))
+        .FirstOrDefault()
+        .InnerText;
+      ParseResult dateParse = ParseDateForYear(htmlDoc.DocumentNode.Descendants("tr")
+        .Where(node => node.FirstChild.InnerText == "Date")
+        .FirstOrDefault().Descendants("td")
+        .FirstOrDefault().InnerText);
+      int year = -1;
+      if (dateParse.Result)
       {
-        Latitude = ParseLatOrLongString(latitude),
-        Longitude = ParseLatOrLongString(longitude),
-        Name = name,
-        Year = year,
-        CategoryId = CategoryId,
-        Url = Url,
-        LastChecked = DateTime.Now,
-        EventId = EventId
-      };
-      db.Entry(newBattle).State = EntityState.Modified;
-      db.SaveChanges();
-      return new ParseResult { Result = true, Message = $"{name} parsed" };
+        year = int.Parse(dateParse.Message);
+      }
+      //* Only save entity to database if it has found a lat, long, and year
+      if (latitude != "" && longitude != "" && name != "" && year != -1)
+      {
+        Battle newBattle = new Battle()
+        {
+          Latitude = ParseLatOrLongString(latitude),
+          Longitude = ParseLatOrLongString(longitude),
+          Name = name,
+          Year = year,
+          CategoryId = CategoryId,
+          Url = Url,
+          LastChecked = DateTime.Now,
+          EventId = EventId
+        };
+        db.Entry(newBattle).State = EntityState.Modified;
+        db.SaveChanges();
+        return new ParseResult { Result = true, Message = $"{name} parsed" };
+      }
+      return new ParseResult { Result = false, Message = $"Unable to parse {name}. Lat: {latitude}. Long: {longitude}. Date: {dateParse.Message}." };
     }
-    return new ParseResult { Result = false, Message = $"Unable to parse {name}. Lat: {latitude}. Long: {longitude}. Date: {dateParse.Message}." };
+    else
+    {
+      return new ParseResult { Result = false, Message = $"Unable to parse {html}" };
+    }
   }
 }
